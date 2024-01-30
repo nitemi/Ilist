5a3d2b68efa53eba8a34340cd32084c3d512ad87
nitemi@ <blessedtgirl@gmail.com>
2024-01-30 13:22:36 +0100

    Add project files.

---
+++ Ilist/Icollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    //ICollection
+    //Dictionary
+    //list, stack, queue,and set
+    public delegate bool newrecord(Collection collection);
+    public class Collection
+    { 
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+            
+    }
+}
---
+++ Ilist/Program.cs
@@ -0,0 +1,66 @@
+﻿using list;
+
+public delegate void Record(int value);
+public class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Collection collection = new Collection()
+        {
+            Id = 65,
+            Name = "Ade",
+            Description = "Professional",
+            Type = "TechSavy",
+
+        };
+        Collection collection1 = new Collection()
+        {
+            Id = 65,
+            Name = "Ade",
+            Description = "Professional",
+            Type = "TechSavy",
+        };
+
+
+
+        //List<string> list = new List<string>();
+        //list.Add("Adunni");
+        //list.Add("Babalola");
+        //list.Add("Comfort");
+        //list.Add("Damilola");
+        //list.Add("Emmanuel");
+
+        //collections(list);
+        //ilists(list);
+        //listdisplay(list);
+
+    }
+    //    public static void collections(ICollection<string> collection)
+    //    {
+    //        collection.Add("Femi");
+    //        foreach (string item in collection)
+    //        {
+    //            Console.WriteLine("The icollection element are :" +  item);
+    //            Console.WriteLine("/////");
+    //        }
+    //    }
+    //    public static void ilists(IList<string> list)
+    //    {
+    //        list.RemoveAt(0);
+    //        foreach (string item in list)
+    //        {
+    //            Console.WriteLine("The ilist element are :" + item);
+    //            Console.WriteLine("/////");
+    //        }
+    //    }
+    //    public static void listdisplay(List<string> mylist)
+    //    {
+    //        //list consist of all the method, inheriting from oth ilist and icollection
+    //        mylist.Sort();
+    //        foreach (string item in mylist)
+    //        {
+    //            Console.WriteLine("The list element are :" + item);
+    //        }
+    //    }
+}
